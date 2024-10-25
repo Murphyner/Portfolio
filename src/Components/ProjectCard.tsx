7d2f199034191e58a1378b02afa0795062f087f1
Agshin <znagshin@gmail.com>
2024-10-26 00:16:18 +0400

    ds

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -13,7 +13,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({time,  title, link, descriptio
     return (
         <Link target="_blank" to={link} className="flex flex-col lg:flex-row gap-5 cursor-pointer hover:shadow-md group text-white font-Poppin transition-all duration-300 ease-in-out p-3 rounded-md hover:bg-gray-600 hover:bg-opacity-10">
             <div className="opacity-50 uppercase text-[13px] pt-[3px]">
-                <img src={time} className="lg:h-14 lg:w-24  rounded-md hover:scale-125 transition-all duration-500 hover:-rotate-6" />
+                <img src={time} className="lg:h-14 lg:w-24  rounded-md hover:scale-125 transition-all duration-500 lg:hover:-rotate-6" />
             </div>
             <div className="w-full lg:w-[75%] flex flex-col group-hover:group gap-2">
                 <Link target="_blank" to={link} className="flex group-hover:text-teal-300 transition-all duration-300 gap-2 items-center">
