331835c7784ec15fa271561b6045cf5830c0e16f
Agshin <znagshin@gmail.com>
2024-10-26 00:04:39 +0400

    fdfd

--- src/Components/Heading.tsx
+++ src/Components/Heading.tsx
@@ -8,7 +8,7 @@ const Heading: React.FC = () => {
         </h2>
       </div>
       <p className="text-xl font-medium font-Poppin tracking-wider">Frontend developer</p>
-      <p className="line-clamp-2 w-[55%] opacity-80 font-Poppin text-sm">"I create responsive, intuitive, and user-friendly interfaces that deliver engaging digital experiences."</p>
+      <p className="w-[55%] opacity-80 font-Poppin text-sm">"I create responsive, intuitive, and user-friendly interfaces that deliver engaging digital experiences."</p>
     </div>
   )
 }
